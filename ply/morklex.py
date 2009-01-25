3e3bf84ef43211dcc6a0812a1580495e1b9cebd1
Kevin Goodsell <kevin-opensource@omegacrash.net>
2009-01-25 12:53:11 -0800

    Adding ply-based lexer.

---
+++ ply/morklex.py
@@ -0,0 +1,90 @@
+import ply.lex as lex
+import re
+import sys
+
+tokens = (
+    # 'Special' tokens
+    'MAGIC',
+    'COMMENT',
+
+    # Common tokens
+    'OBJREF',
+    'OBJID',
+    'LITERAL',
+
+    # Cell tokens
+    'LPAREN',
+    'RPAREN',
+    'COLUMN',
+    'VALUE',
+
+    # Wierd group stuff
+    'GROUPSTART',
+    'GROUPCOMMIT',
+    'GROUPABORT',
+)
+
+states = (
+    ('cell', 'exclusive'),
+)
+
+# 'Special' tokens
+t_MAGIC = r'//\ <!--\ <mdb:mork:z\ v="1\.4"/>\ -->$'
+t_COMMENT = r'//.*$'
+
+# Common tokens
+t_ANY_OBJREF = r'\^[0-9a-fA-F]+'
+
+def t_OBJID(t):
+    r'[0-9a-fA-F]+'
+    return t
+
+def t_LITERAL(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*' # XXX Not really accurate
+    return t
+
+# Cell tokens
+def t_LPAREN(t):
+    r'\('
+    t.lexer.push_state('cell')
+    return t
+
+def t_cell_RPAREN(t):
+    r'\)'
+    t.lexer.pop_state()
+    return t
+
+t_cell_COLUMN = r'''
+    [^^=) \t\n] # Anything but ^, =, ) or whitespace
+    + '''
+
+t_cell_VALUE = r'''=
+    ( [^)\\]  # Anything that's not ) or \
+    | \\\\    # ...or \\
+    | \\\)    # ...or \)
+    )* '''
+
+# Wierd group stuff
+t_GROUPSTART =  r'@\$\$\{[0-9a-fA-F]+\{@'
+t_GROUPCOMMIT = r'@\$\$\}[0-9a-fA-F]+\}@'
+t_GROUPABORT =  r'@\$\$\}~abort~[0-9a-fA-F]+\}@'
+
+# Special rules
+
+t_ANY_ignore = ' \t'
+
+literals = '<>[]{}:'
+
+def t_ANY_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_ANY_error(t):
+    print >> sys.stderr, "Lexing error at line %d, next chars: %r" % (
+        t.lexer.lineno, t.value[:10])
+    t.lexer.skip(1)
+
+lex.lex(reflags=re.MULTILINE)
+
+if __name__ == '__main__':
+    lex.runmain()
