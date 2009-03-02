27a382e3ea0d7e1c2ba0f5755c96bf7adcca2780
Kevin Goodsell <kevin-opensource@omegacrash.net>
2009-03-01 21:38:29 -0800

    Refactoring output filters.
    This is to allow other filter types.

--- src/MorkDB/output/__init__.py
+++ src/MorkDB/output/__init__.py
@@ -1,39 +1,12 @@
 # Copyright (c) 2009 Kevin Goodsell
-import sys
 import os
 
-class BadFilter(StandardError):
-    pass
+import MorkDB.moduleTools as modtools
 
-def getFilter(name):
-    moduleName = 'MorkDB.output.' + name
-    try:
-        exec 'import ' + moduleName
-    except ImportError:
-        raise BadFilter('output filter not found: %s' % name)
+class _OutputFilterPool(modtools.ModulePool):
+    def _validateModule(self, name, module):
+        if not hasattr(module, '_MORK_OUTPUT_FILTER'):
+            raise modtools.BadModule('not a valid output filter: %s' % name)
 
-    module = sys.modules[moduleName]
-    if not hasattr(module, '_MORK_OUTPUT_FILTER'):
-        raise BadFilter('not a valid output filter: %s' % name)
-
-    return module
-
-def iterFilters():
-    filterDir = os.path.dirname(os.path.abspath(__file__))
-    modulesSeen = set()
-
-    for name in os.listdir(filterDir):
-        filtName = os.path.splitext(name)[0]
-        # Skip already seen files
-        if filtName in modulesSeen:
-            continue
-        modulesSeen.add(filtName)
-
-        try:
-            module = getFilter(filtName)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except Exception:
-            continue
-
-        yield (filtName, module)
+filters = _OutputFilterPool(os.path.dirname(os.path.abspath(__file__)),
+                            'MorkDB.output')
