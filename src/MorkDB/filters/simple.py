7932d96048fcef15b58d4c5f39486d57b400a3f2
Kevin Goodsell <kevin-opensource@omegacrash.net>
2010-07-05 12:34:48 -0700

    Replace a few more old-style filters.

---
+++ src/MorkDB/filters/simple.py
@@ -0,0 +1,40 @@
+# Copyright 2010 Kevin Goodsell
+#
+# Simple Mork database filters.
+
+from filterbase import Filter
+
+class StripEmptyCells(Filter):
+    def __init__(self, order):
+        self.mork_filter_order = order
+
+    def add_options(self, parser):
+        parser.add_option('-s', '--strip-empty', action='store_true',
+            help='strip empty cells from the database')
+
+    def process(self, db, opts):
+        if not opts.strip_empty:
+            return
+
+        for (row_namespace, row_id, row) in db.rows.items():
+            for (col, val) in row.items():
+                if not val:
+                    del row[col]
+
+strip_empty_filter = StripEmptyCells(200)
+
+class StripMetaTables(Filter):
+    def __init__(self, order):
+        self.mork_filter_order = order
+
+    def add_options(self, parser):
+        parser.add_option('-m', '--strip-meta', action='store_true',
+            help='strip meta-tables from the database')
+
+    def process(self, db, opts):
+        if not opts.strip_meta:
+            return
+
+        db.metaTables.clear()
+
+strip_metatables_filter = StripMetaTables(100)
