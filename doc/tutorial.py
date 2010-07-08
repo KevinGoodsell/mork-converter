b80b1c1960aff1c50137872e5e8a3ea83fd2cd90
Kevin Goodsell <kevin-opensource@omegacrash.net>
2010-07-08 12:16:01 -0700

    Update filter documentation for new filter style.

--- doc/tutorial.py
+++ doc/tutorial.py
@@ -1,119 +1,122 @@
-# Copyright (c) 2009 Kevin Goodsell
+# Copyright 2009, 2010 Kevin Goodsell
 
-# Tutorial for Writing Output Filters
+# Tutorial for Writing Filters
 #
 # This is a tutorial in the form of a real output filter module -- you can
-# actually drop it into the MorkDB/filter/output directory and use it as-is.
-#
-# The util module contains some handy but optional things. It also contains
-# the ArgumentError exception class, used to indicate problems with the filter
-# arguments. You don't even need this import if you don't want to signal any
-# argument errors, but it's certainly good to have.
-import MorkDB.filter.util as util
-#
-# All output filters require four items: _MORK_OUTPUT_FILTER, description,
-# usage, and output. Descriptions and examples of each follow.
-#
-# _MORK_OUTPUT_FILTER must exist, but its value does not matter. This simply
-# identifies the module as an output filter.
-_MORK_OUTPUT_FILTER = True
-#
-# description must exist, but won't be used if it is empty, None, or otherwise
-# evaluates to False. Should be a basic description of the filter.
-description = 'Tutorial output filter'
-#
-# usage must exist, and provides the names and descriptions for arguments that
-# are accepted by the output filter.
-usage = [
-    ('out', 'Name of file to write to'),
-    ('tabs', 'Use tabs instead of spaces for indentation'),
-]
-#
-# This is the simplest way of providing the arguments, but using util.Argument
-# instances instead of tuples is probably more convenient. Here is what this
-# would look like:
-#usage = [
-#    util.Argument('out', 'Name of file to write to'),
-#    util.Argument('tabs', 'Use tabs instead of spaces for indentation',
-#                  util.convertBool),
-#]
-#
-# Note the addition of util.convertBool, a converter for the 'tabs' argument.
-# This is just a function that takes a string and returns a bool to allow
-# automatic conversion of argument strings to a more useful type. The 'out'
-# parameter needs no conversion, since the expected type is the same as the
-# initial type: str.
-#
-# The convertBool function allows some common words to be given for 'True' or
-# 'False', but more commonly it converts an empty string to 'True'. This allows
-# the option to be given without an explicit value. E.g., the argument 'tabs'
-# has the same effect as 'tabs=yes'.
-#
-# The final required item is the 'output' function, which takes two arguments.
-# The first argument is a MorkDatabase instance. The second argument is a dict
-# of filter arguments with the argument names as the keys and the argument text
-# as the values.
-def output(db, args):
-    # Handling the arguments manually is tedious, which is one reason why you
-    # may want to use util.Argument and util.convertArgs instead.
-    out = None
-    tabs = False
-    for (name, value) in args.items():
-        if name == 'out':
-            out = value
-        elif name == 'tabs':
-            tabs = True
+# actually drop it into the MorkDB/filters directory and use it as-is.
+
+import sys
+
+# The Filter class isn't actually required for writing filters, but it
+# describes the basic interface and is kind of handy.
+from filterbase import Filter
+# EncodingStream is a helper for doing output in whatever character encoding
+# the user asks for.
+from encoding import EncodingStream
+
+# All filters require three items:
+#   mork_filter_order - an integer value used for identifying and ordering
+#                       filters.
+#   add_options       - a function/method that the filter can use to provide
+#                       or modify command-line options.
+#   process           - the function/method that does all the real work.
+
+# It's probably more convenient in general for class *instances* to represent
+# filters, but the class itself can also work. This example will show how this
+# is done. There are several examples of instance-based filters in the source.
+class TutorialFilter(Filter):
+    # The mork_filter_order attribute provides the ordering for filters:
+    # the lower the order, the earlier the filter is run. A negative value
+    # can be used to disable a filter, or in cases where a class is intended
+    # as a base class for filters rather than a filter itself. The 'Filter'
+    # class is an example of this.
+    #
+    # See filterbase.py for a description of the conventions used for order
+    # values.
+    #
+    # Here, we use a low order value in the range of "output" filters, which
+    # begins at 10000. Since generally only one output filter actually does
+    # output, the higher-order filters take precedence (since they have the
+    # final say in setting default options).
+    mork_filter_order = 10001
+
+    # add_options is invoked early during construction of the
+    # optparser.OptionParser instance. This allows filters to provide their
+    # own command-line options, and sometimes to override default option
+    # values. See the optparse documentation for full details of how to add
+    # and use options.
+    #
+    # For an class instance-based filter, this would be a normal method rather
+    # than a classmethod.
+    @classmethod
+    def add_options(cls, parser):
+        # This adds an option '--text' that, if given, sets the option
+        # out_format to the value 'text'. This is the normal way of selecting
+        # an output format.
+        parser.add_option('--text', dest='out_format', action='store_const',
+            const='text', help='write simple text output')
+        # This adds a '--tabs' option that, if given, causes tabs to be used
+        # for indentation.
+        parser.add_option('--tabs', action='store_true',
+            help='use tabs instead of spaces for simple text output')
+
+        # Typically an output filter should set out_format to the value it
+        # recognizes. Higher-order filters will just override it, and the
+        # highest will become the final default. This way when a filter is
+        # removed we gracefully fall back on the next-highest priority filter.
+        parser.set_defaults(out_format='text')
+
+    # process does the real work.
+    @classmethod
+    def process(cls, db, opts):
+        # The process function always gets called for each filter, so it's up
+        # to the filter to check opts and determine if it actually has
+        # anything to do.
+        if opts.out_format != 'text':
+            return
+
+        # That covers the basics of the output filter API. The rest of the work
+        # involves interpreting the MorkDatabase contents and writing the
+        # result. This part is not as straight-forward, because it requires
+        # some understanding of the MorkDatabase class's internals.
+
+        # outname is a common option for describing the output name (file,
+        # directory, or whatever). Here we use '-' or the absence of outname
+        # to select stdout.
+        outname = opts.outname or '-'
+        if outname == '-':
+            f = EncodingStream(opts.out_encoding, sys.stdout)
         else:
-            raise util.ArgumentError('Unknown argument: %s' % name)
+            f = EncodingStream.open(opts.out_encoding, outname)
 
-    if out is None:
-        raise util.ArgumentError("Missing required argument 'out'")
+        if opts.tabs:
+            indent = '\t'
+        else:
+            indent = '    '
 
-    _outputHelper(db, out, tabs)
+        # Basic iteration over tables generally looks like this. db.tables is a
+        # customized dict where the keys are always (namespace, oid) tuples.
+        # The items() method is modified to reflect this, returning the key
+        # components separately.
+        for (namespace, oid, table) in db.tables.items():
+            # This simple example completely skips meta-tables. I don't
+            # recommend this in general, since they may contain useful data.
+            cls._write_table(f, namespace, oid, table, indent)
 
-    # Doing something similar using util.Arguments and util.convertArgs looks
-    # like this:
-    #args = util.convertArgs(usage, args)
-    #if 'out' not in args:
-    #    raise util.ArgumentError("Missing required argument 'out'")
-    #_outputHelper(db, **args)
+    @classmethod
+    def _write_table(cls, f, namespace, oid, table, indent):
+        print >> f, 'TABLE (namespace: %s, id: %s)' % (namespace, oid)
 
-# That covers the basics of the output filter API. The rest of the work
-# involves interpreting the MorkDatabase contents and writing the result. This
-# part is not as straight-forward, because it requires some understanding of
-# the MorkDatabase class's internals.
+        # A table is a customized list type where the items are 3-tuples of
+        # (namespace, id, row). This allows iteration similar to the iteration
+        # over db.tables, but the items() method is not used.
+        for (row_namespace, row_id, row) in table:
+            cls._write_row(f, row_namespace, row_id, row, indent)
 
-def _outputHelper(db, out, tabs=False):
-    f = open(out, 'w')
+    @classmethod
+    def _write_row(cls, f, namespace, oid, row, indent):
+        print >> f, '%sROW (namespace: %s, id: %s)' % (indent, namespace, oid)
 
-    if tabs:
-        indent = '\t'
-    else:
-        indent = '    '
-
-    # Basic iteration over tables generally looks like this. db.tables is a
-    # customized dict where the keys are always (namespace, oid) tuples. The
-    # items() method is modified to reflect this, returning the key components
-    # separately.
-    for (namespace, oid, table) in db.tables.items():
-        # This simple example completely skips meta-tables. I don't recommend
-        # this in general, since they may contain useful data.
-        _writeTable(f, namespace, oid, table, indent)
-
-    f.close()
-
-def _writeTable(f, namespace, oid, table, indent):
-    print >> f, 'TABLE (namespace: %s, id: %s)' % (namespace, oid)
-
-    # A table is a customized list type where the items are 3-tuples of
-    # (namespace, id, row). This allows iteration similar to the iteration
-    # over db.tables, but the items() method is not used.
-    for (rowNamespace, rowId, row) in table:
-        _writeRow(f, rowNamespace, rowId, row, indent)
-
-def _writeRow(f, namespace, oid, row, indent):
-    print >> f, '%sROW (namespace: %s, id: %s)' % (indent, namespace, oid)
-
-    # A MorkRow is a dict mapping column names to cell values.
-    for (column, value) in row.items():
-        print >> f, '%s%s = %s' % (indent * 2, column, value)
+        # A MorkRow is a dict mapping column names to cell values.
+        for (column, value) in row.items():
+            print >> f, '%s%s = %s' % (indent * 2, column, value)
